534d6b4adf891cec4da9b04e1db6d08505efde43
pabloescobargaming18-sketch <pabloescobargaming18@gmail.com>
2025-08-31 01:40:05 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ function convertToBool(text, fault = "true") {
 }
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "PUT YOUER SESON ID WITH OUT NENO-xmd~",
-  OWNER_NUM: process.env.OWNER_NUM || "94726228243",
+  SESSION_ID: process.env.SESSION_ID || "HDASEADL#xzzpunPfIUazyeHfyiHxtATo3ve1GwqWxyESD_MmhC4",
+  OWNER_NUM: process.env.OWNER_NUM || "+221764481954",
   PREFIX: process.env.PREFIX || ".",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
   AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
